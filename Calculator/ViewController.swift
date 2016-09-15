f35fd3ba306104625ea8a07bf4ef4a10510d3591
clarissavazquez <cvasquez-ramo@csumb.edu>
2016-09-15 15:09:40 -0700

    Completed assignment 1 and with the required tasks.
    Walked through the code with the instructor up until lecture 2 and then added the cos, sin, pi, clr, and decimal buttons. Also added a history label under the main display.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var operandStack = Array<Double>()
+    var displayValue: Double {
+        get {
+            if(display.text! != "π") {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            }
+            else {
+                return M_PI
+            }
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
-
-
+    
+    func appendHistory(str: String) {
+        history.text = history.text! + str + " "
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        let str:String = String(format:"%.2f", displayValue)
+        appendHistory(str)
+        print("operandStack: \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        appendHistory(operation)
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        case "cos": performOperation { cos($0) }
+        case "sin": performOperation { sin($0) }
+        default:
+            break
+        }
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        history.text = ""
+        operandStack.removeAll()
+        display.text = "0"
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if(operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if(operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
 }
 
